2aa785e5a572360ba2c28f3211489c12122c38e1
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-03 10:04:54 +0200

    creando metodos en la clase model

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -1,6 +1,21 @@
 package cod.mvc;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Model {
 
+    private ArrayList <Coche> misCoches = new ArrayList<>();
 
+    public ArrayList <Coche> crearCoche(String auxMatricula, String auxModelo, int auxVelocidad){
+
+        misCoches.add(new Coche(auxMatricula, auxModelo, auxVelocidad));
+        return misCoches;
+    }
+
+
+
+    public ArrayList<Coche> getMisCoches() {
+        return misCoches;
+    }
 }
