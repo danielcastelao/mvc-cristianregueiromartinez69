c6264d75d9b8bb0c59f3a5b6b43bab8c1a7c6be3
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-06 08:58:26 +0200

    javadoc de la clase model

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -3,20 +3,44 @@ package cod.mvc;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+/**
+ * clase model con los metodos
+ * @author cristian
+ * @version v1.0
+ */
 public class Model {
 
+    /**
+     * atributo privado ArrayList de tipo Cohe
+     */
     private static ArrayList <Coche> parking = new ArrayList<>();
 
+    /**
+     * Metodo para crear un coche
+     * @param auxMatricula la matricula del coche
+     * @param auxModelo el model del coche
+     * @param auxVelocidad la velocidad del coche
+     * @return
+     */
     public static ArrayList <Coche> crearCoche(String auxMatricula, String auxModelo, int auxVelocidad){
 
         parking.add(new Coche(auxMatricula, auxModelo, auxVelocidad));
         return parking;
     }
 
+    /**
+     * metodo que devuelve el array de coches
+     * @return el array de coches
+     */
     public static ArrayList<Coche> getMisCoches() {
         return parking;
     }
 
+    /**
+     * metodo para cambiar la velocidad del coche según la matrícula proporcionada
+     * @param auxMatricula la matricula que ha de encontrar para encontrar la velocidad
+     * @param velocidad la nueva velocidad del coche
+     */
     public static void cambiarVelocidad(String auxMatricula, int velocidad){
 
         for (Coche coche:parking){
@@ -27,6 +51,11 @@ public class Model {
 
     }
 
+    /**
+     * metodo para obtener la velocidad del coche según la matrícula proporcionada
+     * @param auxMatricula la matrícula del coche que ha de encontrar
+     * @return la velocidad del coche
+     */
     public static int getVelocidad(String auxMatricula){
         int velocidadActual = 0;
         for (Coche coche:parking){
