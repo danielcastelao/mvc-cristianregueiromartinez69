46589caadfb8f55f0b1dcbf4d6ecc0792baaed16
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-10 08:57:38 +0200

    javadoc de la clase main

--- src/cod/mvc/Main.java
+++ src/cod/mvc/Main.java
@@ -1,8 +1,14 @@
 package cod.mvc;
 
+/**
+ * clas emain donde ejecutamos el programa
+ */
 public class Main {
     public static void main(String[] args) {
 
+        /**
+         * hacemos un objeto de la clase vista y llamamos al método muestra velocidad
+         */
          View vista = new View();
          vista.muestraVelocidad("1234A", 120);
     }
