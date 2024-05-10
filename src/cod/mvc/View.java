eeb95270097848f98b42ad9533f9049ba58abec2
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-10 09:42:30 +0200

    añadiendo system out a la view

--- src/cod/mvc/View.java
+++ src/cod/mvc/View.java
@@ -16,6 +16,7 @@ public class View {
      */
     public static boolean muestraVelocidad(String matricula, int velocidad){
 
+        System.out.println(matricula + " : " + velocidad + "km/h");
 
         return true;
     }
