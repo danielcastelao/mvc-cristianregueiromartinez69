4425239a670d9f65e7f8bea4883e12a6d924bac8
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-10 08:56:19 +0200

    javadoc de la clase view

--- src/cod/mvc/View.java
+++ src/cod/mvc/View.java
@@ -1,8 +1,17 @@
 package cod.mvc;
 
-
+/**
+ * Clase view con el metodo de mostrar velocidad
+ * @author cristian
+ * @version v1.0
+ */
 public class View {
 
+    /**
+     * metodo que recibe por parametro la matricula y la velocidad del vehículo
+     * @param matricula la matricula del coche
+     * @param velocidad la velocidad del coche
+     */
     public void muestraVelocidad(String matricula, int velocidad){
 
         System.out.println(matricula + ", Velocidad: " + velocidad + "km/h");
