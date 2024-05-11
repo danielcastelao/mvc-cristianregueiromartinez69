163e8aae7478bd413d2a90ba052469f5ff6b93c8
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-11 10:38:04 +0200

    clase controller actualizada

--- src/cod/mvc/Controler.java
+++ src/cod/mvc/Controler.java
@@ -1,14 +1,36 @@
 package cod.mvc;
 
+/**
+ * clase controller donde tenemos la lógica del programa
+ * @author cristian
+ * @version v1.0
+ */
 public class Controler {
-
+    /**
+     * atributo estático de tipo Model para llamar a los métodos
+     */
+   static Model modelo;
    public static void logicaPrograma(){
 
+       /**
+        * instanciamos el observador
+        */
+       ObserverVelocidad observoVelocidad = new ObserverVelocidad();
+       modelo.addObserver(observoVelocidad);
+
+       /**
+        * creamos coches
+        */
        Model.crearCoche("1234-UH", "Seat", 140);
-       Coche miCoche = Model.getMisCoches().get(0);
+       Model.crearCoche("1235-UH", "Citroen", 90);
+       Model.crearCoche("9876-AB", "Audi", 230);
+
+       /**
+        * le cambiamos la velocidad
+        */
+       modelo.cambiarVelocidad("1235-UH", 100);
 
 
-       System.out.println(View.muestraVelocidad(miCoche.getMatricula(), miCoche.getVelocidad()));
 
    }
 
